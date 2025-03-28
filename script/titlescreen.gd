0fbb28c10b41969213a2cac8df93a5d51d561243
MuffinMan <gran37048@gmail.com>
2025-03-28 10:52:48 +0100

    Clean Up Files & Folders
    SANITATION

--- script/titlescreen.gd
+++ script/titlescreen.gd
@@ -6,10 +6,5 @@ extends Control
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(main_scene)
 
-
-func _on_settings_pressed() -> void:
-	pass # Replace with function body.
-
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
